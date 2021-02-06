4708369bbfc95ec0626b85205c80a606f75b1e38
Noah Cote <noah.cote@gmail.com>
2021-02-06 15:01:49 -0500

    tested and working

--- src/Store/Store.ts
+++ src/Store/Store.ts
@@ -1,12 +1,17 @@
 import { createStore, applyMiddleware, Store } from "redux";
 import { logger } from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import { RootReducer, RootState, RootActionTypes } from "./RootReducer";
 
 const ConfigureStore = (
   initialState?: RootState
 ): Store<RootState, RootActionTypes> => {
-  const store = createStore(RootReducer, initialState, applyMiddleware(logger));
+  const store = createStore(
+    RootReducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(logger))
+  );
 
   return store;
 };

9190a05e771dd75c8f18c4d4e28b47e20a87a9fa
Stephen Suffian <stephen.suffian@gmail.com>
2021-02-06 17:40:15 -0500

    Add an endpoint that supplies the API and phillydb version

--- backend/tests.py
+++ backend/tests.py
@@ -1,10 +1,19 @@
 from unittest import TestCase
-from django.urls import resolve
+from django.urls import resolve, reverse
 from .urls import urlpatterns
+from django.test import Client
+
 
 
 class TestUrls(TestCase):
+    def setUp(self):
+        self.client = Client()
+
     def test_url_resolution(self):
         for urlpattern in urlpatterns:
             route = urlpattern.pattern._route
             assert resolve(f'/{route}') is not None
+
+    def test_api_settings_url(self):
+        response = self.client.get(reverse('settings'))
+        assert 'latest_api_version' in response.json().keys()

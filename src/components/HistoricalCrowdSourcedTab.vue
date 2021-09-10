944529463e18ce1ee64bb31b6d736ced5892367c
Stephen Suffian <stephen.suffian@pm.me>
2021-09-10 00:16:38 -0400

    Update property page

--- src/components/HistoricalCrowdSourcedTab.vue
+++ src/components/HistoricalCrowdSourcedTab.vue
@@ -2,20 +2,25 @@
   <div>
     <sui-container>
       <p v-if="bioResults.length > 0">
+        This currently contains <b>{{ bioResults.length }}</b> existing results. 
+        <sui-button circular icon="exclamation"  @click.native="disclaimerToggle"/>
+        <sui-modal v-model="disclaimerOpen">
+      <sui-modal-header>Crowd-Sourced Information Disclaimer</sui-modal-header>
+      <sui-modal-content>
+          <p>
         The following information was developed and maintained by volunteers.
-        The information includes data from secondary sources, which are not
-        guaranteed to be checked for accuracy and do not necessarily reflect the
-        opinions of any organization or group. No organization, group, or
-        volunteer endorses or accepts responsibility over any external sites
-        that may be linked in this information. The volunteers who contributed
-        to this information make absolutely no guarantees as to the currency,
-        accuracy, completeness, or quality of information written and/or
-        archived on the website, and no organization, group, or volunteer
-        assumes any liability for the currency, accuracy, completeness, or
-        quality of information written and/or archived on the website. The
-        inclusion of names, links and other references is purely on the basis of
-        their relevance to the information. It currently contains
-        <b>{{ bioResults.length }}</b> existing results.
+        The information includes data from secondary sources, which are not guaranteed to be checked for accuracy and do not necessarily reflect the opinions of any organization or group. 
+        No organization, group, or volunteer endorses or accepts responsibility over any external sites that may be linked in this information. 
+        The volunteers who contributed to this information make absolutely no guarantees as to the currency, accuracy, completeness, or quality of information written and/or archived on the website, and no organization, group, or volunteer assumes any liability for the currency, accuracy, completeness, or quality of information written and/or archived on the website. 
+        The inclusion of names, links and other references is purely on the basis of their relevance to the information.
+          </p>
+      </sui-modal-content>
+      <sui-modal-actions>
+        <sui-button positive @click.native="disclaimerToggle">
+          OK
+        </sui-button>
+      </sui-modal-actions>
+    </sui-modal>
       </p>
       <div v-for="(bioResult, i) in bioResults" :key="i">
         <BioResult :bioResult="bioResult" :index="i" />
@@ -69,15 +74,21 @@ export default {
     return {
       modalOpen: false,
       bioResults: [],
+      disclaimerOpen: false,
       airTableUrl:
         "https://airtable.com/embed/shrAacunffP2mP3PC?backgroundColor=orange&prefill_mailing_street=" +
         this.mailingStreet
     };
   },
+  computed: {
+  },
   methods: {
     toggle() {
       this.modalOpen = !this.modalOpen;
-    }
+    },
+    disclaimerToggle() {
+      this.disclaimerOpen = !this.disclaimerOpen;
+    },
   },
   created() {
     getBioTableInfo(this.mailingStreet, this.mailingAddress1).then(data => {

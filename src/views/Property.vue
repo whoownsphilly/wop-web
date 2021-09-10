065856d5bf0b5c591ab7d1b125a9aeeb23f67385
Stephen Suffian <stephen.suffian@pm.me>
2021-09-09 20:56:28 -0400

    Fix top 5 string to only have top N (max 5)

--- src/views/Property.vue
+++ src/views/Property.vue
@@ -51,7 +51,7 @@
                   </span>
                 </sui-accordion-content>
               </sui-accordion>
-              <h3>Top 5 most common 311 complaints by owner</h3>
+              <h3>Top {{ numResultsString(this.complaints) }} most common 311 complaints by owner</h3>
               <p v-if="complaints !== null">
                 <span v-if="complaints.rows.length === 0">
                   No complaints filed.
@@ -67,7 +67,7 @@
                 </span>
               </p>
               <p v-else>Loading...</p>
-              <h3>Top 5 most common violations by owner</h3>
+              <h3>Top {{ numResultsString(this.violations) }}  most common violations by owner</h3>
               <p v-if="violations !== null">
                 <span v-if="violations.rows.length === 0">
                   No violations filed.
@@ -166,10 +166,17 @@ export default {
       latestTransaction: null,
       complaints: null,
       violations: null,
-      fullOwnersList: []
+      fullOwnersList: [],
     };
   },
   computed: {
+    numComplaintsString() { 
+        if(this.complaints){ 
+            return Math.max(this.complaints.rows, 5)
+        } else {
+            return ""
+        }
+    },
     loadingContent() {
       return (
         "Finding all " +
@@ -243,6 +250,16 @@ export default {
       }
       return "";
     }
+  },
+  methods: {
+    numResultsString(theseResults) {
+        if(theseResults && theseResults.rows.length > 0){
+            return Math.min(theseResults.rows.length, 5)
+        } else {
+            return ""
+        }
+    },
+
   },
   async created() {
     this.loading = true;

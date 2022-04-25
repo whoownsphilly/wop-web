d029f1f763cdf089b6cf0393785835d23ee158f3
Stephen Suffian <stephen.suffian@pm.me>
2022-04-24 23:46:14 -0400

    Updated instructions

--- src/views/NeighborhoodsPage.vue
+++ src/views/NeighborhoodsPage.vue
@@ -1,95 +1,58 @@
 <template>
   <div style="font-size: 18px">
-    <p>
-      Select a geographic area to find the 100 properties with the most
-      violations in that area. These are properties that have an active rental
-      license and violations are only counted if they occurred after 2018-01-01.
-    </p>
-    <p>
-      Click <b>"Search By: Zip Code"</b> to enter a zip code as the geographic
-      area of focus. Click <b>"Search By: Map Boundary"</b> to select your own
-      geographic area of choice. You can do this either by zooming in on the
-      map, or <i><b>by clicking one of the shapes on the map legend</b></i> and
-      drawing a custom geographic area.
-    </p>
-    <p>
-      Once you have selected a geographic area, click "Update Map", which will
-      update the markers on the map to represent the 100 properties with the
-      most violations within that area. The list below the map will also
-      populate with this list.
-    </p>
-    <p>
-      To generate custom lists, you first have to use the "+ Add List" tab to
-      create a new list. Currently, each new list automatically gets assigned a
-      different color. The system can reliably handle about 5 lists at a time,
-      but this could be increased in the future. After creating a list, you can
-      click on a property marker in the map, it's address will pop up along with
-      a drop-down menu with the populated list names, and a button that says
-      'Add to List'. By clicking on the address, it will then add that property
-      to your own personal list, which can be subsequently downloaded.
-    </p>
-    <p>
-      After creating a list, you can share out 2 different types of links. There
-      is a 'Link to read-only version of this List' which will link people
-      directly to what is inside the Tab (a copy of the list and a map of just
-      those selected properties). There is also a 'Link to walking directions'
-      which will open up all of the selected properties in an external Google
-      Maps page.
-    </p>
-    <p>
-      If you are working on a list and are not completed with it, you can copy
-      the URL (which changes whenever a property is added) and that URL should
-      return you to the same list that you were working on.
-    </p>
-    <sui-grid>
-      <sui-grid-row>
-        <sui-grid-column :width="2">
-          <label for="search_by">Search By:</label>
-          <sui-form>
-            <sui-form-fields grouped>
-              <sui-form-field>
-                <sui-checkbox
-                  radio
-                  name="search_by"
-                  label="Map Boundary"
-                  value="mapBoundary"
-                  v-model="searchBy"
-                />
-              </sui-form-field>
-              <sui-form-field>
-                <sui-checkbox
-                  radio
-                  name="search_by"
-                  label="Zip Code"
-                  value="zipCode"
-                  v-model="searchBy"
-                />
-              </sui-form-field>
-            </sui-form-fields>
-          </sui-form>
-        </sui-grid-column>
-        <sui-grid-column :width="4">
-          <sui-input
-            v-model="zipCode"
-            v-if="searchBy != 'mapBoundary'"
-            placeholder="Enter zip code here..."
-          />
-        </sui-grid-column>
-        <sui-grid-column :width="6">
-          Filter By: <br />Building Type
-          <sui-dropdown
-            direction="upward"
-            multiple
-            fluid
-            :options="buildingTypes"
-            placeholder="Building Type"
-            search
-            selection
-            v-model="selectedBuildingTypes"
-          />
-        </sui-grid-column>
-      </sui-grid-row>
-    </sui-grid>
+    <instructions-text />
+    <sui-divider horizontal>Search</sui-divider>
+    <sui-container>
+      <sui-grid>
+        <sui-grid-row>
+          <sui-grid-column :width="2">
+            <label for="search_by">Search By:</label>
+            <sui-form>
+              <sui-form-fields grouped>
+                <sui-form-field>
+                  <sui-checkbox
+                    radio
+                    name="search_by"
+                    label="Map Boundary"
+                    value="mapBoundary"
+                    v-model="searchBy"
+                  />
+                </sui-form-field>
+                <sui-form-field>
+                  <sui-checkbox
+                    radio
+                    name="search_by"
+                    label="Zip Code"
+                    value="zipCode"
+                    v-model="searchBy"
+                  />
+                </sui-form-field>
+              </sui-form-fields>
+            </sui-form>
+          </sui-grid-column>
+          <sui-grid-column :width="4">
+            <sui-input
+              v-model="zipCode"
+              v-if="searchBy != 'mapBoundary'"
+              placeholder="Enter zip code here..."
+            />
+          </sui-grid-column>
+          <sui-grid-column :width="6">
+            Filter By: <br />Building Type
+            <sui-dropdown
+              direction="upward"
+              multiple
+              fluid
+              :options="buildingTypes"
+              placeholder="Building Type"
+              search
+              selection
+              v-model="selectedBuildingTypes"
+            />
+          </sui-grid-column>
+        </sui-grid-row>
+      </sui-grid>
+    </sui-container>
     <sui-divider horizontal>
       <sui-button v-on:click="updatePropertyList">Update Map</sui-button>
     </sui-divider>
@@ -140,7 +103,7 @@
         :attached="false"
       >
         <sui-button v-on:click="addSelectionToSelectedPropertyList"
-          >Button</sui-button
+          >Add map bounds to list</sui-button
         >
         <property-list :name="name" :propertyList="thisList" />
       </sui-tab-pane>
@@ -150,6 +113,7 @@
 <script>
 import LeafletMapNeighborhood from "@/components/ui/LeafletMapNeighborhood";
 import PropertyList from "@/components/page/neighborhoods/PropertyList";
+import InstructionsText from "@/components/page/neighborhoods/InstructionsText";
 import DataTable from "@/components/ui/DataTable";
 import {
   getNeighborhoodsPageInfo,
@@ -162,6 +126,7 @@ export default {
     LeafletMapNeighborhood,
     DataTable,
     PropertyList,
+    InstructionsText,
   },
   data() {
     return {

e5aba1eb11ab802835450cef6237ea77aa42bdb4
Stephen Suffian <stephen.suffian@pm.me>
2023-02-28 20:03:11 -0500

    Remove database

--- backend/settings.py
+++ backend/settings.py
@@ -83,12 +83,7 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = {}
 
 
 # Password validation

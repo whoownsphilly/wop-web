7190f5c3662f40f08d79d89bac2c409d9bad7fa3
keith <keith@wattcarbon.com>
2023-08-02 16:11:12 -0400

    added conditional title

--- solid/src/components/Header.tsx
+++ solid/src/components/Header.tsx
@@ -1,13 +1,19 @@
-import {Component} from "solid-js";
+import {Component, createMemo, Show} from "solid-js";
+import {useLocation} from "@solidjs/router";
 
 const Header: Component = () => {
+    const location = useLocation();
+    const isHome = createMemo(() => location.pathname === "/");
+
 return (
-    <header class="flex justify-between p-4 text-xs md:text-sm lg:text-base">
+    <header class="flex justify-between items-center p-4 text-xs md:text-sm lg:text-base">
         <div class="flex gap-4 items-center">
             <a class="px-4 py-2" href="/">Home</a>
             <a class="px-4 py-2" href="/neighborhoods">Neighborhoods</a>
         </div>
-        <div>Who owns Philly?</div>
+        <Show when={!isHome()}>
+            <div class="text-xl text-bold text-emerald-800">Who owns Philly?</div>
+        </Show>
         <div class="flex gap-4 items-center">
             <a class="px-4 py-2 bg-emerald-700 rounded-lg text-white font-bold" href="/take-action">Take Action</a>
             <a href="/About">About</a>
--- solid/src/routes/properties/PropertyBase.tsx
+++ solid/src/routes/properties/PropertyBase.tsx
@@ -6,29 +6,13 @@ import {getPropertyBasicsPageInfo, getPropertyLatestOwnerDetailsInfo} from "../.
 
 const PropertyBase: Component = (props) => {
     const params = useParams();
-    // const [property, setProperty] = createSignal(new Property())
-    // const [owner, setOwner] = createSignal({})
-    // const date = new Date(2006, 6, 1)
-    createEffect(async () => {
-        // const result= await getPropertyBasicsPageInfo(params.id, date)
-        // setProperty(Property.toProperty(result))
-        // const result2 = await getPropertyLatestOwnerDetailsInfo(params.id, date)
-        // setOwner(result2)
-        // setLink(result2.street_view_link)
-    });
-
     return (
-        <main class="w-full">
-        {/*<h2 class="text-center text-2xl mb-4">*/}
-        {/*    <span class="font-bold">{property().location}</span>*/}
-        {/*    <span class="mx-2" >is likely owned by</span>*/}
-        {/*    <span class="font-bold">{owner().latest_owner}</span>*/}
-        {/*</h2>*/}
-        <nav class="flex w-full my-2 gap-4 ml-4">
-            <a class="nav-button" href={"/properties/" + params.id }>Summary</a>
-            <a class="nav-button" href={"/properties/" + params.id + "/timeline" }>Timeline</a>
-            <a class="nav-button" href={"/properties/" + params.id + "/owner"}>Owner</a>
-        </nav>
+        <main class="w-full mt-4">
+            <nav class="flex w-full my-2 gap-4 ml-4">
+                <a class="nav-button" href={"/properties/" + params.id }>Summary</a>
+                <a class="nav-button" href={"/properties/" + params.id + "/timeline" }>Timeline</a>
+                <a class="nav-button" href={"/properties/" + params.id + "/owner"}>Owner</a>
+            </nav>
             <section class="border-t rounded-sm p-4 flex justify-between gap-4">
             {props.children}
             </section>

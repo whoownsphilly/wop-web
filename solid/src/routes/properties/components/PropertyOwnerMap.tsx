730a2c63590f241632d8935629f26fd96cbacd80
keith <keith@wattcarbon.com>
2023-08-09 16:23:18 -0400

    added clusters

--- solid/src/routes/properties/components/PropertyOwnerMap.tsx
+++ solid/src/routes/properties/components/PropertyOwnerMap.tsx
@@ -5,8 +5,14 @@ const PropertyOwnerMap: Component = (props) => {
     props = mergeProps({}, props);
 
     createEffect( () =>{
-        const lngLats = props.properties.map(p => {
-            return [p.lng, p.lat]
+        const propertyFeatures = props.properties.map(p => {
+            return { "type": "Feature",
+                    properties: {},
+                    geometry: {
+                        type: "Point",
+                        coordinates: [p.lng, p.lat]
+                    },
+                }
         })
         mapboxgl.accessToken = "pk.eyJ1Ijoib3Jhbmdlbm90b3JhbmdlIiwiYSI6ImNsbDB4YWlldDF6ODUzZ3BycjM3eHlnbTUifQ.HciS95bDZaZlOb-pncOA1w";
         const map = new mapboxgl.Map({
@@ -16,22 +22,66 @@ const PropertyOwnerMap: Component = (props) => {
             zoom: 12 // starting zoom
         })
         map.on("load", () => {
-            console.log(lngLats)
-            if(lngLats.length > 0) {
+
+            if(propertyFeatures.length > 0) {
                 map.addSource("properties", {
                     type: "geojson",
                     data:  {
-                        type: "Feature",
-                        geometry: {
-                            type: "LineString",
-                            coordinates: lngLats
-                        },
+                        "type": "FeatureCollection",
+                        "crs": { "type": "name", "properties": {} },
+                        "features": propertyFeatures
                     },
+                    cluster: true,
+                    clusterMaxZoom: 14, // Max zoom to cluster points on
+                    clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+                });
+                map.addLayer({
+                    id: "clusters",
+                    type: "circle",
+                    source: "properties",
+                    filter: ["has", "point_count"],
+                    paint: {
+                    // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+                    // with three steps to implement three types of circles:
+                    //   * Blue, 20px circles when point count is less than 100
+                    //   * Yellow, 30px circles when point count is between 100 and 750
+                    //   * Pink, 40px circles when point count is greater than or equal to 750
+                        "circle-color": [
+                            "step",
+                            ["get", "point_count"],
+                            "#6ee7b7",
+                            100,
+                            "#34d399",
+                            750,
+                            "#047857"
+                        ],
+                        "circle-radius": [
+                            "step",
+                            ["get", "point_count"],
+                            20,
+                            100,
+                            30,
+                            750,
+                            40
+                        ]
+                    }
+                });
+                map.addLayer({
+                    id: "cluster-count",
+                    type: "symbol",
+                    source: "properties",
+                    filter: ["has", "point_count"],
+                    layout: {
+                        "text-field": ["get", "point_count_abbreviated"],
+                        "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+                        "text-size": 12
+                    }
                 });
                 map.addLayer({
                     id: "unclustered-point",
                     type: "circle",
                     source: "properties",
+                    filter: ["!", ["has", "point_count"]],
                     paint: {
                         "circle-color": "#047857",
                         "circle-radius": 4,
@@ -40,7 +90,31 @@ const PropertyOwnerMap: Component = (props) => {
                     }
                 });
 
-                console.log(map)
+                // inspect a cluster on click
+                map.on("click", "clusters", (e) => {
+                    const features = map.queryRenderedFeatures(e.point, {
+                        layers: ["clusters"]
+                    });
+                    const clusterId = features[0].properties.cluster_id;
+                    map.getSource("properties").getClusterExpansionZoom(
+                        clusterId,
+                        (err, zoom) => {
+                            if (err) return;
+
+                            map.easeTo({
+                                center: features[0].geometry.coordinates,
+                                zoom: zoom
+                            });
+                        }
+                    );
+                });
+
+                map.on("mouseenter", "clusters", () => {
+                    map.getCanvas().style.cursor = "pointer";
+                });
+                map.on("mouseleave", "clusters", () => {
+                    map.getCanvas().style.cursor = "";
+                });
             }
 
         })

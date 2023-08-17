3f32b5ca7e08491b3021d37066eb592841d7f728
keith <keith@wattcarbon.com>
2023-08-17 10:19:19 -0400

    more styling

--- solid/src/routes/neighborhoods/Neighborhoods.tsx
+++ solid/src/routes/neighborhoods/Neighborhoods.tsx
@@ -148,47 +148,40 @@ const Neighborhoods: Component = () => {
 
     return (<main class="flex flex-col justify-between gap-1">
         <section class="w-full flex justify-between gap-2 py-2 px-4">
-            <div class="flex flex-col gap-1 w-1/4 overflow-auto h-[200px]">
+            <div class="flex flex-col gap-1 w-1/4 overflow-auto h-[150px]">
                 <h3 class="font-medium text-xl">Search</h3>
-                <h4>Searching Properties</h4>
-                <div class="hidden">
-                    Select a geographic area to find the 100 properties with the most violations in that area. These are properties that have an active rental license and violations are only counted if they occurred after 2018-01-01.
+                <h4 class="text-sm">Searching Properties</h4>
+                <Show when={false}>
+                    <div class="text-sm">
+                        Select a geographic area to find the 100 properties with the most violations in that area. These are properties that have an active rental license and violations are only counted if they occurred after 2018-01-01.
 
-                    Click "Search By: Zip Code" to enter a zip code as the geographic area of focus. Click "Search By: Map Boundary" to select your own geographic area of choice. You can do this either by zooming in on the map, or by clicking one of the shapes on the map legend and drawing a custom geographic area.
+                        Click "Search By: Zip Code" to enter a zip code as the geographic area of focus. Click "Search By: Map Boundary" to select your own geographic area of choice. You can do this either by zooming in on the map, or by clicking one of the shapes on the map legend and drawing a custom geographic area.
 
-                    Once you have selected a geographic area, click "Update Map", which will update the markers on the map to represent the 100 properties with the most violations within that area. The list below the map will also populate with this list.
-                </div>
-                <h4>Generating Lists</h4>
-                <div class="hidden">
+                        Once you have selected a geographic area, click "Update Map", which will update the markers on the map to represent the 100 properties with the most violations within that area. The list below the map will also populate with this list.
+                    </div>
+                </Show>
+                <h4 class="text-sm">Generating Lists</h4>
+                <Show when={false}>
+                <div>
                     To generate custom lists, you first have to use the "+ Add List" tab to create a new list. Currently, each new list automatically gets assigned a different color. The system can reliably handle about 5 lists at a time, but this could be increased in the future. After creating a list, you can click on a property marker in the map, it's address will pop up along with a drop-down menu with the populated list names, and a button that says 'Add to List'. By clicking on the address, it will then add that property to your own personal list, which can be subsequently downloaded.
 
                     You can also bulk add properties to the list by going to that list's tab, using the selector tool on the map legend, drawing a shape on the map, and then clicking the 'Add map bounds to list' button.
                 </div>
-                <h4>Sharing Lists</h4>
-                <div class="hidden">
+                </Show>
+                <h4 class="text-sm">Sharing Lists</h4>
+                <Show when={false}>
+                <div>
                     After creating a list, you can share out 2 different types of links. There is a 'Link to read-only version of this List' which will link people directly to what is inside the Tab (a copy of the list and a map of just those selected properties). There is also a 'Link to walking directions' which will open up all of the selected properties in an external Google Maps page.
 
                     If you are working on a list and are not completed with it, you can copy the URL (which changes whenever a property is added) and that URL should return you to the same list that you were working on.
                 </div>
+                </Show>
             </div>
-            <div class="flex flex-col gap-1 w-1/4">
-                <h3  class="font-medium text-xl">List</h3>
-                <div class="flex justify-between items-center gap-2">
-                    <div class="text-sm">Units per list</div>
-                    <input type="text" class="border w-1/3 text-right px-2 py-1 rounded-sm" value={numUnitsPerList()} onChange={(e) => {setNumUnitsPerList(e.target.value)}}/>
-                </div>
-                <div  class="flex justify-between items-center gap-2">
-                    <div class="text-sm">Number of lists</div>
-                    <input type="text" class="border w-1/3 text-right px-2 py-1 rounded-sm" value={numLists()} onChange={(e) => {setNumLists(e.target.value)}}/>
-                </div>
-
-
-            </div>
-            <div class="flex flex-col gap-1 w-1/4">
+            <div class="flex flex-col gap-1 w-1/2">
                 <h3 class="font-medium text-xl">Building Type</h3>
                 <div class="flex flex-wrap">
                     <For each={buildingTypes}>{(type) =>
-                        <button class={styles["toggle-button"]} classList={{'w-1/2': true, [active]: isActiveBuildingType(type.value)}} onClick={toggleActiveBuildingType(type)} >{type.text}</button>
+                        <button class={styles["toggle-button"]} classList={{'w-1/3': true, [active]: isActiveBuildingType(type.value)}} onClick={toggleActiveBuildingType(type)} >{type.text}</button>
                     }
                     </For>
                 </div>
@@ -208,8 +201,8 @@ const Neighborhoods: Component = () => {
         <section class="flex items-center border-t border-b px-4">
             <div class="font-medium uppercase px-4 py-2 whitespace-nowrap">Search By</div>
             <div class="flex justify-between py-2 border-l w-full ">
-                <div class="flex">
-                    <select class="py-2 px-4 mx-2 pr-9 block w-full border-gray-200 rounded-md text-sm w-[150px]" name={searchType()}  onChange={(e) => {  setSearchType(e.target.value)}}>
+                <div class="flex justify-start">
+                    <select class="py-2 px-4 mx-2 pr-9 block w-full border-gray-200 rounded-md text-sm w-[200px]" name={searchType()}  onChange={(e) => {  setSearchType(e.target.value)}}>
                         <option selected value="mapBoundary" >Map</option>
                         <option value="address" >Address</option>
                         <option value="zipCode" >Zip Code</option>
@@ -222,36 +215,45 @@ const Neighborhoods: Component = () => {
                     <Show when={isSearchType("address")}>
                         <div class="flex">
                             <div class="w-2/3 flex items-center gap-2 grow mr-2">
-                                <div class="text-sm text-gray-400">Starting Address</div>
-                                <input type="text" class="border w-1/3 p-2 rounded-sm grow" value={startingAddress()} onChange={(e) => {setStartingAddress(e.target.value)}}/>
+                                <input type="text" class="border w-1/3 p-2 rounded-sm grow" placeholder="Starting Address" value={startingAddress()} onChange={(e) => {setStartingAddress(e.target.value)}}/>
                             </div>
                             <div class="w-1/3 flex items-center gap-2">
-                                <div class="text-sm whitespace-nowrap text-gray-400">Blocks from Address</div>
                                 <input type="text" class="border w-full p-2 w-10 text-right" value={blocksFromAddress()} onChange={(e) => {setBlocksFromAddress(e.target.value)}}/>
+                                <div class="text-sm whitespace-nowrap text-gray-400">Blocks from Address</div>
                             </div>
                         </div>
 
                     </Show>
                     <Show when={isSearchType("zipCode")}>
                         <div class="flex items-center gap-2">
-                            <input type="text" class="border w-full p-2" value={zipCode()} onChange={(e) => {setZipCode(e.target.value)}}/>
-                            <div class="text-sm whitespace-nowrap text-gray-400">Zip Code</div>
+                            <input type="text" class="border w-full p-2" placeholder="Zip Code" value={zipCode()} onChange={(e) => {setZipCode(e.target.value)}}/>
                         </div>
                     </Show>
                 </div>
-                <button class="bg-emerald-700 px-4 py-2 text-white rounded-lg mr-4" onclick={update}>Update Map</button>
+
             </div>
 
         </section>
 
         <section class="w-full flex gap-4 h-[600px] px-4">
-            <div class="w-1/4">
-
+            <div>
+                <div class="flex justify-between items-center gap-2">
+                    <div class="text-sm">Units per list</div>
+                    <input type="text" class="border w-1/3 text-right px-2 py-1 rounded-sm" value={numUnitsPerList()} onChange={(e) => {setNumUnitsPerList(e.target.value)}}/>
+                </div>
+                <div  class="flex justify-between items-center gap-2">
+                    <div class="text-sm">Number of lists</div>
+                    <input type="text" class="border w-1/3 text-right px-2 py-1 rounded-sm" value={numLists()} onChange={(e) => {setNumLists(e.target.value)}}/>
+                </div>
+                <button class="bg-emerald-700 px-4 py-2 text-white rounded-sm mr-4 w-full mt-2" onclick={update}>Update Map</button>
                 <h3 class="text-xl font-medium">Searched Properties</h3>
                 <For each={walkingLists()}>{(walkingList) =>
                     <div>L {walkingList.location}</div>
                 }
                 </For>
+                <Show when={walkingLists().length ===  0}>
+                    <div class="text-center bg-gray-100 p-4">No Results</div>
+                </Show>
             </div>
             <div class="w-3/4">
                 <NeighborhoodMap properties={properties()} />

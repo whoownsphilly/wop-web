e0e78394b8e6235a4b919da9445f522a83f35337
Stephen Suffian <stephen.suffian@pm.me>
2025-03-16 20:49:22 -0400

    Add loading indicators

--- src/components/properties/PropertyTimeline.vue
+++ src/components/properties/PropertyTimeline.vue
@@ -1,84 +1,102 @@
 <script setup lang="ts">
-
-import {onMounted} from "vue";
-import {getPropertyDetailsPageInfo} from "../../services/apiFetcher.ts";
-import ApexCharts from "apexcharts";
+import { onMounted, ref } from 'vue'
+import { getPropertyDetailsPageInfo } from '../../services/apiFetcher.ts'
+import ApexCharts from 'apexcharts'
 
 const props = defineProps({
-  id: String
+  id: String,
 })
+
+const isLoading = ref(true)
+
 onMounted(async () => {
-  const propertyResults = await getPropertyDetailsPageInfo(props.id)
+  isLoading.value = true
+  try {
+    const propertyResults = await getPropertyDetailsPageInfo(props.id)
 
-  const timelineData = propertyResults["property_ownership_timeline"].map((data) => {
-    return { x: data.owner, y: [ new Date(data.start.substring(0,10)).getTime(), new Date(data.end.substring(0,10)).getTime() ]}
-  })
-  const propertyValuePurchaseData = propertyResults["property_value_timeline"]
-      .filter(data => data.status === "purchased").map((data) => {
-        return { x: new Date(data.date.substring(0,10)).toLocaleString(), y: data.property_value}
+    const timelineData = propertyResults['property_ownership_timeline'].map((data) => {
+      return {
+        x: data.owner,
+        y: [
+          new Date(data.start.substring(0, 10)).getTime(),
+          new Date(data.end.substring(0, 10)).getTime(),
+        ],
+      }
+    })
+    const propertyValuePurchaseData = propertyResults['property_value_timeline']
+      .filter((data) => data.status === 'purchased')
+      .map((data) => {
+        return { x: new Date(data.date.substring(0, 10)).toLocaleString(), y: data.property_value }
       })
 
-  const propertyValueAssesedData = propertyResults["property_value_timeline"]
-      .filter(data => data.status === "assessed").map((data) => {
-        return { x: new Date(data.date.substring(0,10)).toLocaleString(), y: data.property_value}
+    const propertyValueAssesedData = propertyResults['property_value_timeline']
+      .filter((data) => data.status === 'assessed')
+      .map((data) => {
+        return { x: new Date(data.date.substring(0, 10)).toLocaleString(), y: data.property_value }
       })
 
-  const timelineChart = new ApexCharts(document.querySelector('#timeline'), {
-    series: [
-      {
-        data: timelineData
-      }
-    ],
-    chart: {
-      height: 250,
-      type: 'rangeBar'
-    },
-    plotOptions: {
-      bar: {
-        horizontal: true
-      }
-    },
-    xaxis: {
-      type: 'datetime'
-    }
-
-  })
-
-  const propertyValueChart= new ApexCharts(document.querySelector('#propertyValue'), {
-    series: [
-      {
-        data: propertyValuePurchaseData
+    const timelineChart = new ApexCharts(document.querySelector('#timeline'), {
+      series: [
+        {
+          data: timelineData,
+        },
+      ],
+      chart: {
+        height: 250,
+        type: 'rangeBar',
       },
-      {
-        data: propertyValueAssesedData
+      plotOptions: {
+        bar: {
+          horizontal: true,
+        },
       },
-    ],
-    chart: {
-      height: 250,
-      type: 'area'
-    },
-    xaxis: {
-      type: 'datetime'
-    }
+      xaxis: {
+        type: 'datetime',
+      },
+    })
 
-  })
+    const propertyValueChart = new ApexCharts(document.querySelector('#propertyValue'), {
+      series: [
+        {
+          data: propertyValuePurchaseData,
+        },
+        {
+          data: propertyValueAssesedData,
+        },
+      ],
+      chart: {
+        height: 250,
+        type: 'area',
+      },
+      xaxis: {
+        type: 'datetime',
+      },
+    })
 
-
-  timelineChart.render().then()
-  propertyValueChart.render().then()
+    await Promise.all([timelineChart.render(), propertyValueChart.render()])
+  } catch (error) {
+    console.error('Error loading timeline data:', error)
+  } finally {
+    isLoading.value = false
+  }
 })
 </script>
 
 <template>
-  <div class="flex flex-col w-full">
-    <h2>Who owned this property since 2000</h2>
-    <section class="w-full">
-      <div>
-        <div id="timeline"></div>
+  <div>
+    <div v-if="isLoading" class="mb-4 flex justify-between rounded-sm bg-gray-200 p-4">
+      <div class="w-full text-center">
+        <span>Loading timeline data...</span>
       </div>
-      <div>
-        <div id="propertyValue"></div>
-      </div>
-    </section>
+      <div
+        class="h-5 w-5 animate-spin rounded-full border-2 border-emerald-800 border-t-transparent"
+      ></div>
+    </div>
+    <template v-else>
+      <div id="timeline"></div>
+      <div id="propertyValue"></div>
+    </template>
   </div>
 </template>
+
+<style scoped></style>
